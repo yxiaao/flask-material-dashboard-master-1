bb723e6b417ce5ef9b595cfa644c07469288fbd2
Mark Xia <markxia@codefreesoft.com>
2022-07-05 22:31:25 +0800

    update review

--- apps/home/routes.py
+++ apps/home/routes.py
@@ -2,6 +2,7 @@
 """
 Copyright (c) 2019 - present AppSeed.us
 """
+import json
 
 from apps.home import blueprint
 from flask import render_template, request
@@ -9,25 +10,32 @@ from flask_login import login_required
 from jinja2 import TemplateNotFound
 from docx import Document
 import jsonlines
-import en_core_web_lg
 
+# Entity_link model
+import en_core_web_lg
+# Contract Review model
 from legal_annotator.annotator import Annotator
-import json
-import torch
-from transformers import AutoTokenizer, AutoModelForQuestionAnswering, pipeline
+# Similarity model
+# import ast
+# from retrieval_code_master.retrieval_demo_class import Retriever
+
+# Load Models
+# Entity Link
 nlp = en_core_web_lg.load()
 nlp.add_pipe('dbpedia_spotlight')
 document_Data = {}
 has_processed = 'false'
-
+# Contract Review
 LM_checkpoint_path = 'legal_annotator/roberta-base'
 anno_info_path = 'legal_annotator/CUAD_anno_info.txt'
 anno = Annotator(LM_checkpoint_path=LM_checkpoint_path, anno_info_path=anno_info_path)
+# Similarity
+# model = Retriever()
+
 
 @blueprint.route('/index')
 @login_required
 def index():
-
     return render_template('home/index.html', segment='index', document_selected='false', processed='false')
 
 
@@ -41,7 +49,8 @@ def processFile():
         document_Data['result'] = handleContract(request.form['content'])
         has_processed = 'true'
         document_Data['content'] = request.form['content']
-        return render_template("home/entityLink.html", segment='entityLink', document_selected='true', processed=has_processed, documentData=document_Data)
+        return render_template("home/entityLink.html", segment='entityLink', document_selected='true',
+                               processed=has_processed, documentData=document_Data)
 
 
 @blueprint.route('/upload', methods=['GET', 'POST'])
@@ -62,6 +71,7 @@ def handleFile():
             document_Data['content'] += (paragraph.text + '\n')
     return render_template('home/document.html', segment='document', document_selected='true', processed='false',
                            documentData=document_Data)
+
 
 @blueprint.route('/kg')
 @login_required
@@ -120,9 +130,9 @@ def handleContract(content):
     processResult['Summarize'] = handleSummarize(content)
     processResult['Review'] = handleReview(content)
     # processResult['KnowledgeGraph'] = handleKnowledgeGraph(content)
-    # print('finish analyze')
-    # return processResult
+    # processResult['Similarity'] = handleSimilarity(content)
     return processResult
+
 
 def handleReview(content):
     review = []
@@ -132,9 +142,9 @@ def handleReview(content):
     text = content
 
     result = anno(text)
-    print(result)
     # output = convert_html_element(text, result) + convert_html_element_table(result)
     return result
+
 
 def handleSummarize(content):
     paragraph = []
@@ -147,36 +157,103 @@ def handleSummarize(content):
             paragraph.append(row)
     return {'para': paragraph, 'sum': summarized}
 
+
+def handleSimilarity(content):
+    if content['retrieved_function'] == "similar":
+        sentence_list = []
+        text = request.form['text']
+        sentence_list.append(text)
+        neighbor_str_list, neighbor_score_list = model.get_ann_sent_recommend(sentence_list)
+        print(neighbor_str_list)
+        print(neighbor_score_list)
+        if type(neighbor_str_list) == str:
+            result_html = neighbor_str_list
+        else:
+            result_html = convert_html_retrieve_table(neighbor_str_list, neighbor_score_list)
+        print(result_html)
+
+        return render_template("Contract_similarity.html", original_input=text, result=result_html)
+
+    if content['retrieved_function'] == "supplementary":
+
+        # sentence_list =[]
+        text = content['text']
+        sentence_list = ast.literal_eval(text)
+        neighbor_str_list = model.get_supplementary_sent(sentence_list)
+        print(neighbor_str_list)
+        if type(neighbor_str_list) == str:
+            result_html = neighbor_str_list
+        else:
+            result_html = convert_html_supple_table(neighbor_str_list)
+        print(result_html)
+        return render_template("Contract_similarity.html", original_input=text, result=result_html)
+
+
 def handleEntityLink(content):
-    # entityLinkResult = open(
-    #     './apps/static/result/output_2ThemartComInc_19990826_10-12G_EX-10.10_6700288_EX-10.10_Co-Branding Agreement_ '
-    #     'Agency Agreement.txt').read().replace(
-    #     '(', '').replace(')', '')
-    # entityLinkResult = entityLinkResult.splitlines(keepends=False)
-    # for i in range(0, len(entityLinkResult)):
-    #     entityLinkResult[i] = entityLinkResult[i].replace('\'', '').split(',')
-    #     for j in range(0, len(entityLinkResult[i])):
-    #         entityLinkResult[i][j] = entityLinkResult[i][j].lstrip()
-    # return entityLinkResult
     doc = nlp(content)
+    # pre_result = convert_html(doc)
+    linkInfo = doc.to_json()['spans']['dbpedia_spotlight']
+    # {start, end, label, kb_id}
+    print(linkInfo)
+    return linkInfo
 
-    pre_result = convert_html(doc)
-    print(pre_result)
-    return pre_result
+    # return pre_result
 
-def convert_html(doc):
+
+def convert_html(doc, result):
     last_idx = 0
     children = []
     for ent in doc.ents:
         if ent.kb_id_ != '':
             children.append(doc.text[last_idx:ent.start_char])
             children.append(
-                entity(doc.text[ent.start_char:ent.end_char],ent.kb_id_))
+                entity(doc.text[ent.start_char:ent.end_char], ent.kb_id_))
             last_idx = ent.end_char
             if ent == doc.ents[-1]:
                 children.append(doc.text[last_idx:])
+
     return ' '.join(children)
 
+
 def entity(entity_text, entity_link):
-    linking_html = "<span class = 'highlight' url=" +'"'+ entity_link +'"' + ' target="_blank">' + entity_text + "</span>"
+    linking_html = "<span class = 'highlight' url=" + '"' + entity_link + '"' + ' target="_blank">' + entity_text + "</span>"
     return linking_html
+
+
+### Retrieve task
+def convert_html_retrieve_table(result, score):
+    output = []
+    contract_elem_start = '<table>\n<caption> Similar Contract Retrieved </caption>\n<thead>\n<tr>\n<th scope="col">Score</th>\n<th scope="col"> Retrieved Contract Sentence </th>\n</tr>\n</thead>\n<tbody>\n'
+    contract_elem_last = '</tbody>\n</table>'
+    output.append(contract_elem_start)
+    for i in range(len(result[0])):
+        output.append(table_retrieve(result[0][i], score[0][i]))
+    output.append(contract_elem_last)
+    return ''.join(output)
+
+
+def table_retrieve(sent, score):
+    sentence_html = '<tr>\n<th scope="row">' + str(round(score, 2)) + '</th>\n<td>' + sent + '</td>\n</tr>\n'
+    return sentence_html
+
+
+### supplementary task
+def convert_html_supple_table(result):
+    output = []
+    contract_elem_start = '<table>\n<caption> Supplementary Contract Retrieved </caption>\n<thead>\n<tr>\n<th scope="col"> Supplementary Contract Sentence </th>\n</tr>\n</thead>\n<tbody>\n'
+    contract_elem_last = '</tbody>\n</table>'
+    output.append(contract_elem_start)
+    for supp_sent in result:
+        output.append(table_supple(supp_sent))
+    output.append(contract_elem_last)
+    return ''.join(output)
+
+
+def table_supple(sent):
+    sentence_html = '<tr>\n<td>' + sent + '</td>\n</tr>\n'
+    return sentence_html
+
+
+
+
+
